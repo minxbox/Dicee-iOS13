ff8980d3954e480cfebbdd59b8b57c856fd5aaf0
minxbox2@gmail.com <>
2022-10-28 17:48:33 +0900

    .

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    //var leftDiceNumber = 1
+    //var rightDiceNumber = 5
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //who.what = value
+        diceImageView1.image = UIImage(named: "DiceOne")
+//        diceImageView1.alpha = 0.5
+        
+        diceImageView2.image = UIImage(named: "DiceOne")
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        var diceArray = [UIImage(named: "DiceOne"),UIImage(named: "DiceTwo"),UIImage(named: "DiceThree"),UIImage(named: "DiceFour"),UIImage(named: "DiceFive"),UIImage(named: "DiceSix")]
+        //print("leftDiceNumber at beginning = \(leftDiceNumber)")
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        //leftDiceNumber += 1
+        //rightDiceNumber -= 1
+        
+        //Int.random(in: 1...10)
+        //print("leftDiceNumber at the end = \(leftDiceNumber)")
+        
+    }
 }
 
