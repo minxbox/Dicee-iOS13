caaeb28bdb618ba0a457aa9781e7889620ee89fd
minxbox2@gmail.com <>
2022-10-31 11:11:21 +0900

    randomise the dice images

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,28 +14,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    //var leftDiceNumber = 1
-    //var rightDiceNumber = 5
     override func viewDidLoad() {
         super.viewDidLoad()
         //who.what = value
         diceImageView1.image = UIImage(named: "DiceOne")
-//        diceImageView1.alpha = 0.5
-        
         diceImageView2.image = UIImage(named: "DiceOne")
     }
 
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        var diceArray = [UIImage(named: "DiceOne"),UIImage(named: "DiceTwo"),UIImage(named: "DiceThree"),UIImage(named: "DiceFour"),UIImage(named: "DiceFive"),UIImage(named: "DiceSix")]
-        //print("leftDiceNumber at beginning = \(leftDiceNumber)")
-        diceImageView1.image = diceArray[Int.random(in: 0...5)]
-        diceImageView2.image = diceArray[Int.random(in: 0...5)]
-        //leftDiceNumber += 1
-        //rightDiceNumber -= 1
         
-        //Int.random(in: 1...10)
-        //print("leftDiceNumber at the end = \(leftDiceNumber)")
+        let diceArray = [UIImage(named: "DiceOne"),UIImage(named: "DiceTwo"),UIImage(named: "DiceThree"),UIImage(named: "DiceFour"),UIImage(named: "DiceFive"),UIImage(named: "DiceSix")]
+        
+        diceImageView1.image = diceArray[Int.random(in: 0...5)] //randomElement
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
         
     }
 }
